8d0b50026a5aa79f0535d9e2f3b63c4dc740b659
Mattias Eyherabide <mattias.eyherabide@protonmail.com>
2022-02-01 20:13:44 +0100

    Finished base twing and add homepage image in assets folder

--- src/Controller/WebSiteController.php
+++ src/Controller/WebSiteController.php
@@ -2,7 +2,17 @@
 
 namespace App\Controller;
 
-class WebSiteController
-{
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+class WebSiteController extends AbstractController {
+
+
+    /**
+     * @Route("/")
+     */
+     public function home(){
+         return $this->render('website/homepage.html.twig');
+     }
 }
